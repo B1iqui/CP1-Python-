63da4b42547f5aa2bdfedcf82269a1975f6ce4cf
Bogdan Koykov <92939131+B1iqui@users.noreply.github.com>
2021-11-02 19:11:51 +0100

    Delete 21.py

--- 21.py
+++
@@ -1,9 +0,0 @@
-import random
-
-a = random.randint(1, 6)
-b = random.randint(1, 6)
-c = random.randint(1, 6)
-
-sum_of_digits = a + b + c
-
-print(f'Result: {a}, {b}, {c}. Sum of digits: {sum_of_digits}')
