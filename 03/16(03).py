59f91d62609d1f45c3d2fe2aa1a244f59992db95
Bogdan Koykov <92939131+B1iqui@users.noreply.github.com>
2021-11-04 19:13:27 +0100

    Add files via upload

---
+++ 03/16(03).py
@@ -0,0 +1,7 @@
+n = int(input('Enter first number: '))
+z = int(input('Enter second number: '))
+
+if n > z:
+        print(f'Numbers in ascending order: {n}, {z}')
+else:
+        print(f'Numbers in ascending order: {z}, {n}')
---
+++ 03/31(03).py
@@ -0,0 +1,5 @@
+import random
+
+for i in range(1, 21):
+    a = random.randint(5,10)
+    print(i, '-', a)
