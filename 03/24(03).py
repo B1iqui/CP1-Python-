2007c294cac4f398f907c2dc59c7682da189e1be
Bogdan Koykov <92939131+B1iqui@users.noreply.github.com>
2021-11-06 22:42:47 +0100

    Update 24(03).py

--- 03/24(03).py
+++ 03/24(03).py
@@ -1,8 +1,8 @@
 for i in range(5):
     for j in range(i+1):
-        print('*', end = '')
+        print('*', end = ' ')
     print()
-for o in range(5,1,-1):
-    for k in range(o-1):
-        print('*', end = '')
-    print()
+for j in range(5, 1, -1):
+    for l in range(j - 1):
+        print('*', end = ' ')
+    print()    
