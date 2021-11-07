2af596d8f6c02e33520b4c358fee4ed85110a62d
Bogdan Koykov <92939131+B1iqui@users.noreply.github.com>
2021-11-07 11:14:43 +0100

    Add files via upload

---
+++ 04 During Classes/13(04).py
@@ -0,0 +1,4 @@
+array = [8, 2, 5, 1, 9]
+
+for digit in array:
+    print(digit ** 2)
---
+++ 04 During Classes/14(04).py
@@ -0,0 +1,9 @@
+array = ['Genowefa', 'Onufry', 'Celestyna', 'Alojzy', 'Pankracy']
+m_length = 0
+maximum = []
+for letter in array:
+    if len(letter) > m_lenght:
+        m_length == len(letter)
+        maximum.append(letter)
+    
+print(maximum)
