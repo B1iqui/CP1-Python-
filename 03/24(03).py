75d2ece02aa527abc77fec3a2f231265aa6e1e96
Bogdan Koykov <92939131+B1iqui@users.noreply.github.com>
2021-11-16 22:03:00 +0100

    Delete 03 directory

--- 03/24(03).py
+++
@@ -1,8 +0,0 @@
-for i in range(5):
-    for j in range(i+1):
-        print('*', end = ' ')
-    print()
-for j in range(5, 1, -1):
-    for l in range(j - 1):
-        print('*', end = ' ')
-    print()    
--- 03/25(03).py
+++
@@ -1,7 +0,0 @@
-a = int(input())
-b = int(input())
-
-print('*' * b)
-for i in range(a - 2):
-    print('*', ' ' * (b - 4), '*')
-print('*' * b)
