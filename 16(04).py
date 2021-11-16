aafb1ec15a75d7cc33634164a3bbe4fb8443e56f
Bogdan Koykov <92939131+B1iqui@users.noreply.github.com>
2021-11-16 21:59:41 +0100

    Delete 16(04).py

--- 16(04).py
+++
@@ -1,7 +0,0 @@
-def is_month(num):
-    months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
-    return months[num - 1]
-
-n = int(input())
-
-print(is_month(n))
